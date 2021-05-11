bbd1d138e4493374310709bd5de5ed67ceb25756
CunningFox146 <makar.papca@gmail.com>
2021-05-11 14:41:49 +0300

    Added login and register views

---
+++ WpfOrganizer/WpfOrganizer/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfOrganizer.ViewModels
+{
+    class LoginViewModel : ViewModel
+    {
+
+    }
+}
---
+++ WpfOrganizer/WpfOrganizer/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfOrganizer.ViewModels
+{
+    class RegisterViewModel : ViewModel
+    {
+
+    }
+}
