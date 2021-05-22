8300edd69fe08bcad6db5e29494ac9fc9436ef49
CunningFox146 <makar.papca@gmail.com>
2021-05-23 00:28:14 +0300

    Tags rework

---
+++ WpfOrganizer/WpfOrganizer/Converters/TagNameTrimmer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfOrganizer.Converters
+{
+    class TagNameTrimmer : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string name = (string)value;
+            if (name == null) return value;
+
+            return name[0].ToString().ToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
+    }
+}
