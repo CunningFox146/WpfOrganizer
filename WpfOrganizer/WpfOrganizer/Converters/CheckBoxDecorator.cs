33b540553221e15ac9ba0a74f13902110b8040ba
CunningFox146 <makar.papca@gmail.com>
2021-05-23 16:08:54 +0300

    Added strikethrough for checked items

---
+++ WpfOrganizer/WpfOrganizer/Converters/CheckBoxDecorator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using WpfOrganizer.Models;
+
+namespace WpfOrganizer.Converters
+{
+    class CheckBoxDecorator : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isChecked = (bool)value;
+            return isChecked ? TextDecorations.Strikethrough : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
+    }
+}
